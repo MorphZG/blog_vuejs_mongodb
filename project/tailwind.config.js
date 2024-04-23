8fec5a94e3f1ab9149b02642c1561183767630ea
MorphZG <zoran1607@gmail.com>
2024-04-23 12:14:47 +0200

    add google fonts

--- project/tailwind.config.js
+++ project/tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/*.{vue,js}",
+    "./src/views/*.{vue,js,ts,jsx,tsx}",
+    "./src/components/*.{vue,js,ts,jsx,tsx}"
+  ],
   theme: {
-    extend: {}
+    extend: {},
+    fontFamily: {
+      robotoMono: "roboto-mono",
+      cabin: "cabin",
+      lekton: "lekton"
+    }
   },
   plugins: []
 };
