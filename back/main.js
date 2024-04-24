33d4ed95ee723e3624f6d0eefbe5d886f1d44a4e
MorphZG <zoran1607@gmail.com>
2024-04-24 19:15:54 +0200

    update imports

--- back/main.js
+++ back/main.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import Post from "./PostModel.js";
+import Post from "./models/Post.js";
 
 const app = express();
 // if front and back end are hosted on different domains
