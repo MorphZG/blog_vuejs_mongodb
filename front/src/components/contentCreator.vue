15b08049b297dd6d1e60b0d29aaa24a761321794
MorphZG <zoran1607@gmail.com>
2024-04-28 22:35:40 +0200

    update

--- front/src/components/contentCreator.vue
+++ front/src/components/contentCreator.vue
@@ -20,25 +20,32 @@ async function postData() {
 </script>
 
 <template>
-  {{ title }}
-  {{ content }}
   <form class="" @submit.prevent="postData" method="post">
     <input
-      class="m-5 block h-10 border-2 text-slate-900 shadow-2xl"
+      class="m-5 block h-10 rounded border-2 text-slate-900 shadow-2xl"
       v-model="title"
       type="text"
       name="title"
       id="title-input"
+      size="60"
       placeholder="title"
     />
     <textarea
-      class="m-5 block border-2 text-slate-900 shadow-2xl"
+      class="m-5 block rounded border-2 text-slate-900 shadow-2xl"
       v-model="content"
       name="content"
       id="content-input"
+      cols="60"
+      rows="10"
       placeholder="content"
     ></textarea>
-    <button class="size- mx-10 bg-red-900" @submit.prevent="postData" type="submit">Submit</button>
+    <button
+      class="m-5 block rounded bg-neutral-800 px-2 py-1 font-cabin text-white hover:bg-neutral-700"
+      @submit.prevent="postData"
+      type="submit"
+    >
+      Submit
+    </button>
   </form>
 </template>
 
--- front/src/views/ContentView.vue
+++ front/src/views/ContentView.vue
@@ -1,5 +1,6 @@
 <script setup>
 import store from "@/store";
+import { ref } from "vue";
 
 function getWordCount(string) {
   let words = string.split(" ");
