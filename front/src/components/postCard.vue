b7f459be994a4ba060b78c7470a2c867678b834d
MorphZG <zoran1607@gmail.com>
2024-05-08 11:19:42 +0200

    split postCard component, display post as a card

---
+++ front/src/components/postCard.vue
@@ -0,0 +1,57 @@
+<script setup>
+import store from "@/store";
+import { ref } from "vue";
+
+// content view should display a list of post cards, where each card is single post
+
+function getWordCount(string) {
+  let words = string.split(" ");
+  let count = words.length;
+  return count;
+}
+
+function getReadingTime(content) {
+  let words = getWordCount(content);
+  let wordsMinute = 180;
+  return Math.ceil(words / wordsMinute);
+}
+
+const props = defineProps({ test: String });
+</script>
+
+<template>
+  <main>
+    <!--list content here-->
+    <div
+      class="hoverFx m-1 cursor-pointer rounded-lg border-2 border-rose-500 bg-neutral-800 p-5"
+      v-for="(post, itemIndex) in store.posts"
+      :key="itemIndex"
+      @click="$router.push(`/content/${post.id}`)"
+    >
+      <h1 class="mb-2 text-center font-robotoMono text-xl font-bold">{{ post.title }}</h1>
+      <p class="text-center font-lekton">reading time: {{ getReadingTime(post.content) }} min</p>
+      <p>{{ test }}</p>
+    </div>
+  </main>
+</template>
+
+<style lang="css" scoped></style>
+
+<!--
+  <main>
+    <div
+      class="module-wrap flex w-full flex-row flex-wrap content-center justify-center rounded-lg"
+    >
+      <div
+        class="hoverFx m-1 w-full cursor-pointer rounded-lg border-2 border-rose-500 bg-neutral-800 p-5"
+        v-for="(post, itemIndex) in store.posts"
+        :key="itemIndex"
+        @click="$router.push(`/content/${post.id}`)"
+      >
+        <h1 class="mb-2 text-center font-robotoMono text-xl font-bold">{{ post.title }}</h1>
+        <p class="font-lekton">Average reading time: {{ getReadingTime(post.content) }} min</p>
+      </div>
+    </div>
+  </main>
+
+-->
--- front/src/views/ContentView.vue
+++ front/src/views/ContentView.vue
@@ -1,36 +1,13 @@
 <script setup>
-import store from "@/store";
+import postCard from "@/components/postCard.vue";
 import { ref } from "vue";
-
-function getWordCount(string) {
-  let words = string.split(" ");
-  let count = words.length;
-  return count;
-}
-
-function getReadingTime(content) {
-  let words = getWordCount(content);
-  let wordsMinute = 180;
-  return Math.ceil(words / wordsMinute);
-}
 </script>
 
 <template>
-  <main>
-    <!--list content here-->
-    <h1 class="ml-10 font-roboto text-2xl font-thin">>> Featured Posts</h1>
-    <div class="flex">
-      <div
-        class="m-2 max-w-lg cursor-pointer border-2 border-rose-500 bg-neutral-800 p-5"
-        v-for="(post, itemIndex) in store.posts"
-        :key="itemIndex"
-        @click="$router.push(`/content/${post.id}`)"
-      >
-        <h1 class="mb-2 text-center font-robotoMono text-xl font-bold">{{ post.title }}</h1>
-        <p class="font-lekton">Average reading time: {{ getReadingTime(post.content) }} min</p>
-      </div>
-    </div>
-  </main>
+  <h1 class="ml-10 font-roboto text-2xl font-thin">>> Featured Posts</h1>
+  <div class="flex items-center justify-center">
+    <postCard class="flex" />
+  </div>
 </template>
 
 <style lang="css" scoped></style>
